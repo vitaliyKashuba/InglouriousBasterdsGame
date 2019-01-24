ceeb6f4ba2bbe474256c2794cc65b6556fd6d6f4
Vitaliy <k.vitaliy1994@gmail.com>
2019-01-24 09:24:32 +0200

    add qr's

--- src/main/java/springApplication/Main.java
+++ src/main/java/springApplication/Main.java
@@ -3,29 +3,14 @@ package springApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
-//import org.telegram.telegrambots.starter.EnableTelegramBots;
 
 @SpringBootApplication
-//@EnableTelegramBots
 public class Main
 {
 
     public static void main(String[] args)
     {
         ApiContextInitializer.init();
-
-//        TelegramBotsApi botsApi = new TelegramBotsApi();
-
         SpringApplication.run(Main.class, args);
-
-//        ApiContextInitializer.init();
-
-//        try
-//        {
-//            botsApi.registerBot(bot);
-//        } catch (TelegramApiException e)
-//        {
-//            e.printStackTrace();
-//        }
     }
 }
