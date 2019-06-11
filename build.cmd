a03fa373cc9c6b4889b7a5309bc7d1f9bf4de26a
Vitaliy <k.vitaliy1994@gmail.com>
2019-06-11 12:22:02 +0300

    try to add build script

---
+++ build.cmd
@@ -0,0 +1,8 @@
+cd front
+npm run-script build
+echo front build succeed
+#move dist/front ../src/main/resources
+#../src/main/resources/front ../src/main/resources/static
+#echo moved
+#ping 127.0.0.1 -n 6 > nul
+#set /p DUMMY=Hit ENTER to continue...
