aad40a1ced5758b922bc2c99d2e2f39e401db305
Vitaliy <k.vitaliy1994@gmail.com>
2019-06-11 15:53:44 +0300

    another one deploy test

--- build.cmd
+++
@@ -1,8 +0,0 @@
-cd front
-npm run-script build
-echo front build succeed
-#move dist/front ../src/main/resources
-#../src/main/resources/front ../src/main/resources/static
-#echo moved
-#ping 127.0.0.1 -n 6 > nul
-#set /p DUMMY=Hit ENTER to continue...
