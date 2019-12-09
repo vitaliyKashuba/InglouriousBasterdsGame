5f2a1cdb5617022b3a0b0a3bb9769e81178f30e5
Vitaliy Kashuba <k.vitaliy1994@gmail.com>
2019-12-09 10:59:20 +0200

    test heroku datasource

--- src/main/java/springApplication/config/DatasourceConfig.java
+++ src/main/java/springApplication/config/DatasourceConfig.java
@@ -1,21 +1,43 @@
 package springApplication.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+@Slf4j
 @Configuration
 public class DatasourceConfig {
 
+//    @Bean
+//    public DataSource getDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.url("jdbc:postgresql://localhost:5001/ib");
+//        dataSourceBuilder.username("root");
+//        dataSourceBuilder.password("123123");
+//        return dataSourceBuilder.build();
+//    }
+
     @Bean
-    public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:postgresql://localhost:5001/ib");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("123123");
-        return dataSourceBuilder.build();
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        log.debug(dbUri.toString());
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        DataSourceBuilder basicDataSource = DataSourceBuilder.create();;
+        basicDataSource.url(dbUrl);
+        basicDataSource.username(username);
+        basicDataSource.password(password);
+
+        return basicDataSource.build();
     }
 
 }
