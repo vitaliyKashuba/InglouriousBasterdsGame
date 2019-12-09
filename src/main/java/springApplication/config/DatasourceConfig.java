8f082d964efcf9502dddafe147d6adc7c704d0b2
Vitaliy Kashuba <k.vitaliy1994@gmail.com>
2019-12-09 13:28:17 +0200

    cleanup

--- src/main/java/springApplication/config/DatasourceConfig.java
+++ src/main/java/springApplication/config/DatasourceConfig.java
@@ -13,25 +13,23 @@ import java.net.URISyntaxException;
 @Configuration
 public class DatasourceConfig {
 
-//    @Bean
-//    public DataSource getDataSource() {
-//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.url("jdbc:postgresql://localhost:5001/ib");
-//        dataSourceBuilder.username("root");
-//        dataSourceBuilder.password("123123");
-//        return dataSourceBuilder.build();
-//    }
-
+    /**
+     * copy-pasted from hwroku dev-center
+     *
+     * to keep in secret db credentials (not push to github in .properties, but take from heroku env)
+     * local test env is "postgres://root:123123@localhost:5001/ib"
+     *
+     * @return
+     * @throws URISyntaxException
+     */
     @Bean
     public DataSource dataSource() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//        URI dbUri = new URI("postgres://root:123123@localhost:5001/ib");
 
         System.out.println("DB URI " + dbUri.toString());
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
         DataSourceBuilder basicDataSource = DataSourceBuilder.create();;
